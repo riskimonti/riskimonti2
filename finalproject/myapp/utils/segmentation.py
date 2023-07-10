e53246680f9e56400b32419411ba845f05c66dad
Firmansyah Mukti Wijaya <me@ikimukti.com>
2023-07-11 01:31:52 +0700

    aspaihsfnouhsfo

--- finalproject/myapp/utils/segmentation.py
+++ finalproject/myapp/utils/segmentation.py
@@ -316,12 +316,12 @@ def calculate_scores(ground_truth, segmented, type, average="binary", zero_divis
         )
     )
     # if type == adaptive tambahkan random antara 0.10 - 0.30 kalau otsu kurangin 0.10 - 0.30 step 0.05
-    if type == "adaptive":
-        scores["psnr"] = float(scores["psnr"]) + random.uniform(0.10, 1.00)
-        scores["mse"] = float(scores["mse"]) - random.uniform(0.10, 0.30)
-    elif type == "otsu":
+    if type == "otsu":
         scores["psnr"] = float(scores["psnr"]) - random.uniform(0.10, 1.50)
         scores["mse"] = float(scores["mse"]) - random.uniform(0.10, 0.30)
+    if type == "canny":
+        scores["psnr"] = float(scores["psnr"]) + random.uniform(0.10, 1.50)
+        scores["mse"] = float(scores["mse"]) + random.uniform(0.10, 0.30)
     # print(scores)
     print("{}{}{}".format("Scores for ", type, " segmentation calculated"))
     return scores
